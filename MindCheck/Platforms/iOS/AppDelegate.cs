5193be232055d33c93d72ec97b71fa7e6017ac1d
Bondarenko <Honor@ZOMBIE>
2024-08-26 12:20:21 +0600

    Add project files.

---
+++ MindCheck/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MindCheck
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
